4d7a24c8c4f4408de039d943429d83d09f8c4a06
Rahat Santosh <r.santosh@sms.ed.ac.uk>
2024-07-24 23:32:08 +0100

    Aliasing Script

---
+++ test_run.sh
@@ -0,0 +1,1 @@
+python3 src/main.py --config=mappo --env-config=gymma with env_args.time_limit=25 env_args.key="mpe:SimpleSpread-v0" opponent_modelling=True opponent_model_decode_observations=True opponent_model_decode_actions=True latent_dims=32
