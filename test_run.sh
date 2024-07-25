0e69fce44bc2cbb54acaa98d4c6f870344dd3928
Rahat Santosh <r.santosh@sms.ed.ac.uk>
2024-07-25 02:47:48 +0100

    Include LBF simple env command

--- test_run.sh
+++ test_run.sh
@@ -1,1 +1,5 @@
 python3 src/main.py --config=mappo --env-config=gymma with env_args.time_limit=25 env_args.key="mpe:SimpleSpread-v0" opponent_modelling=True opponent_model_decode_observations=True opponent_model_decode_actions=True latent_dims=32
+
+# Simple env runs for sanity checks etc.
+python3 src/main.py --config=mappo --env-config=gymma with env_args.time_limit=25 env_args.key="lbforaging:Foraging-8x8-2p-3f-v2" common_reward=False opponent_modelling=True opponent_model_decode_observations=True opponent_model_decode_actions=True latent_dims=32
+
