5467222a85e9642dc360404406ed7d54bd4ce174
Rahat Santosh <r.santosh@sms.ed.ac.uk>
2024-08-05 13:08:10 +0100

    Smaller test runs

---
+++ src/slurm_exp_script_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=epymarl_batch
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=160:00:00
+#SBATCH --mem-per-cpu=2000M
+#
+#SBATCH --array=0-12%20
+#SBATCH --requeue
+#SBATCH --nice
+
+source ~/anaconda3/bin/activate
+conda activate am
+
+python search.py run --config epymarl_experiment_config_test.yaml --seeds=3 single $SLURM_ARRAY_TASK_ID
